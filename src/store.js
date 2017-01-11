addaeca4bdbf29fbb86d0a1f2de521585ed66d77
bigzhu <vermiliondun@gmail.com>
2017-01-11 18:26:49 +0800

    不在/下,不要记录last

--- src/store.js
+++ src/store.js
@@ -266,8 +266,10 @@ export const actions = {
         entries => {
           for (let entry of entries) {
             let message = entry.target.__vue__.message
-            console.log(entry)
-            console.log(entry.target)
+            if (entry.target.__vue__.$route.path !== '/') {
+              continue
+            }
+            console.log(entry.target.__vue__.$route.path)
             dispatch('recordLastMessage', message.created_at)
             state.io.unobserve(entry.target)
           }
@@ -476,8 +478,6 @@ export const actions = {
     var parm = { last_time: time }
     return dispatch('put', {url: '/api_last', body: parm, loading: false}).then(function (data) {
       state.unread_message_count = data.unread_message_count
-      console.log(data)
-      console.log(state.unread_message_count)
       commit('SET_LAST_TIME', parseInt(time, 10))
       commit('REFRESH_LOCAL_UNREAD_MESSAGE_COUNT')
       // 如果<20了，就预加载一些
