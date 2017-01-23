c6f7f81545f2a4e2a1278802a06bd4402f8e0064
bigzhu <vermiliondun@gmail.com>
2017-01-23 16:34:52 +0800

    用最高清,可能有的图会不存在

--- src/components/Instagram.vue
+++ src/components/Instagram.vue
@@ -16,7 +16,9 @@
     computed: {
       img_url: function () {
         let img_url = this.message.extended_entities.url
-        img_url = img_url.replace('s640x640', 's1080x1080').replace('s750x750', 's1080x1080').replace('/e35/', '')
+        console.log(img_url)
+        img_url = img_url.replace('s640x640', 's1080x1080').replace('s750x750', 's1080x1080') // .replace('/e35/', '/').replace('/e15/', '/')
+        console.log(img_url)
         img_url = window.btoa(window.btoa(img_url))
         return '/api_sp/' + img_url
       },
